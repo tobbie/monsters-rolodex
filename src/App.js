828639e7d91e6cf1dece25fc5122f1203ee41ea7
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2020-11-28 17:46:44 +0100

    added search input button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField:''
            
     }
   }
@@ -18,10 +19,20 @@ class App extends Component{
   }
    
   render(){
+    const {monsters, searchField} = this.state
+    const filteredMonsters = 
+          monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+                                                     
+                                                      
     return (
       <div className="App">
-      <CardList monsters = {this.state.monsters} />
-    </div>
+          <input type='search' placeholder="search monsters" 
+              onChange={event => 
+                this.setState({searchField: event.target.value})         
+              } />
+
+         <CardList monsters = {filteredMonsters} />
+      </div>
     
     ); 
   }
